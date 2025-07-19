81539b718e72320bb54b348c65dacde8d29ed173
Mehdi <sckapa.bst@gmail.com>
2025-07-19 15:28:22 +0200

    fix config and vars

--- hosts/mehdinix/home.nix
+++ hosts/mehdinix/home.nix
@@ -38,7 +38,7 @@
     ../../home/system/udiskie
     ../../home/system/clipman
 
-    ./secrets # CHANGEME: You should probably remove this line, this is where I store my secrets
+    #./secrets
   ];
 
   home = {
@@ -84,9 +84,7 @@
       cbonsai
       pipes
       cmatrix
-
-      # Backup
-      brave
+      
       vscode
     ];
 
